6b1044c942a922453a6f658055c5414288301f75
eduardo-takeo <dudu.takeo96@gmail.com>
2020-02-15 11:04:39 -0300

    added bcrypt module

--- face-recognition-brain-api/server.js
+++ face-recognition-brain-api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const app = express();
+const bcrypt = require('bcrypt');
 
 app.use(express.json());
 const database = {
@@ -38,6 +38,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
+    bcrypt.hash(password, 10, function(err, hash) {
+        console.log(hash);
+    });
     database.users.push({
         id: '125',
         name: name,
@@ -48,6 +51,43 @@ app.post('/register', (req, res) => {
     })
     res.json(database.users[database.users.length - 1]);
 })
+
+app.get('/profile/:id', (req, res) => {
+    const {id} = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id) {
+            found = true;
+            return res.json(user);            
+        }
+    })
+    if(!found) {
+        res.status(404).json('no such user');
+    }
+})
+
+app.get('/image', (req, res) => {
+    const {id} = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);            
+        }
+    })
+    if(!found) {
+        res.status(400).json('user was not found');
+    }
+})
+
+// Load hash from your password DB.
+// bcrypt.compare(myPlaintextPassword, hash, function(err, result) {
+//     // result == true
+// });
+// bcrypt.compare(someOtherPlaintextPassword, hash, function(err, result) {
+//     // == false
+// });
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
